e847a9679b9988e985a7145682942efc24655728
aliensarefake <guoxiaoyu2003429@gmail.com>
2024-10-10 16:52:09 +0800

    create header component

---
+++ frontend/src/Homepage/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header-main">
+        <div className="header-text">
+            USDC/ETH Pool Tracker
+        </div>
+    </div>
+  );
+}
+
+export default Header;
