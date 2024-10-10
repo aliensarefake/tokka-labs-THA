d48a362be6000812e353ca19fd816dcc5d736ff7
aliensarefake <guoxiaoyu2003429@gmail.com>
2024-10-10 16:51:52 +0800

    create homepage

---
+++ frontend/src/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import './Homepage.css';
+import Header from './Header/Header';
+
+function Homepage() {
+    return (
+        <div className="homepage-main">
+            <Header/>
+        </div>  
+    );
+}
+
+export default Homepage;
