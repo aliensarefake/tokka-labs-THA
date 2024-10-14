7dd20f5117e1882fe6f3c4a0b0bcabe0ce4f666c
aliensarefake <guoxiaoyu2003429@gmail.com>
2024-10-14 12:35:18 +0800

    fix mongodb connection bug

--- backend/app.js
+++ backend/app.js
@@ -7,18 +7,15 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/batch', require('./routes/batch'));
-app.use('/transactions', require('./routes/transactions'));
+app.use('/batch', require('./routes/batch.js'));
+app.use('/transactions', require('./routes/transaction.js'));
 
-mongoose.connect(config.get('MONGO_URI'), {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(config.get('MONGO_URI'))
   .then(() => {
     console.log('MongoDB connected');
-    liveTransactionService.startLiveTransactionListener();
+    liveTransactionService.startLiveTransactionListener(); 
   })
   .catch((err) => console.error('MongoDB connection error:', err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
