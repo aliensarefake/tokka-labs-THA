e7cb00bcb3671ac992536501cec92c97059744c8
aliensarefake <guoxiaoyu2003429@gmail.com>
2024-10-14 22:58:39 +0800

    update header value

--- frontend/src/Homepage/TxnForm/formComponents/TxnFormHeader.js
+++ frontend/src/Homepage/TxnForm/formComponents/TxnFormHeader.js
@@ -7,7 +7,7 @@ const TxnFormHeader = () => {
       <th>Transaction Hash</th>
       <th>From Address</th>
       <th>To Address</th>
-      <th>Transaction Fee (USDC)</th>
+      <th>Transaction Fee (USDT)</th>
       <th>Transaction Fee (ETH)</th>
       <th>Timestamp</th>
     </tr>
