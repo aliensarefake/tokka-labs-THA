33cdbbcf0e35d1dbe1590fe63fbfb25e0b65d18a
aliensarefake <guoxiaoyu2003429@gmail.com>
2024-10-14 13:59:32 +0800

    use cor middleware

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('config');
 const liveTransactionService = require('./services/liveTransactionService.js');
 
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Routes
