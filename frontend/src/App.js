51404598f90d044373a6507de90a4b10da4b0c1f
aliensarefake <guoxiaoyu2003429@gmail.com>
2024-10-14 14:01:05 +0800

    integrate batch processing logic into FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import './index.css';
+import React, { useEffect } from 'react';
+import { startBatchPolling } from './batchProcessor';
 
 import Homepage from './Homepage/Homepage';
 
 function App() {
+  useEffect(() => {
+    startBatchPolling(); 
+  }, []); 
+
   return (
     <div className="App">
       <Homepage/>
---
+++ frontend/src/batchProcessor.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+let currentEndTime = new Date();
+const batchInterval = 60 * 60 * 1000;
+const pollingInterval = 3 * 60 * 1000; 
+
+const processBatch = async () => {
+  const currentStartTime = new Date(currentEndTime.getTime() - batchInterval);
+
+  try {
+    console.log('inside try');
+    await axios.post('http://localhost:4100/batch', {
+      startTime: currentStartTime.toISOString(),
+      endTime: currentEndTime.toISOString(),
+    });
+
+    console.log(`Processed batch from ${currentStartTime} to ${currentEndTime}`);
+
+    currentEndTime = currentStartTime;
+  } catch (error) {
+    console.error('Batch processing error:', error);
+  }
+};
+
+export const startBatchPolling = () => {
+    console.log("NINNN")
+  processBatch();
+
+  setInterval(processBatch, pollingInterval);
+};
